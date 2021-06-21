028449969fd1056e73f8727d442a506761889e97
vsoch <vsoch@users.noreply.github.com>
2021-06-21 16:34:56 -0600

    adding basic testing for types
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- test/source/allocation.cpp
+++ test/source/allocation.cpp
@@ -19,13 +19,79 @@ auto get_one(smeagle::Corpus const& corpus, char const* name) {
   return funcs;
 }
 
-TEST_CASE("Register Allocation") {
+TEST_CASE("Register Allocation and Types") {
   auto corpus = smeagle::Smeagle("liballocation.so").parse();
 
   SUBCASE("one int") {
-    auto funcs = get_one(corpus, "f1");
+    auto funcs = get_one(corpus, "test_type_int");
     REQUIRE(funcs.size() == 1);
     auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "int");
+    CHECK(parameters[0].location == "%rdi");
+  }
+
+  SUBCASE("one string") {
+    auto funcs = get_one(corpus, "test_type_string");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "string");
+    CHECK(parameters[0].location == "%rdi");
+  }
+
+  SUBCASE("one char") {
+    auto funcs = get_one(corpus, "test_type_char");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "char");
+    CHECK(parameters[0].location == "%rdi");
+  }
+
+  SUBCASE("one bool") {
+    auto funcs = get_one(corpus, "test_type_boolean");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "bool");
+    CHECK(parameters[0].location == "%rdi");
+  }
+
+  SUBCASE("void") {
+    auto funcs = get_one(corpus, "test_type_void");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    REQUIRE(parameters.size() == 0);
+  }
+
+  SUBCASE("one wchar_t") {
+    auto funcs = get_one(corpus, "test_type_wchar_t");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "wchar_t");
+    CHECK(parameters[0].location == "%rdi");
+  }
+
+  SUBCASE("one float") {
+    auto funcs = get_one(corpus, "test_type_float");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "float");
+    CHECK(parameters[0].location == "%xmm7");
+  }
+
+  SUBCASE("one double (double floating point)") {
+    auto funcs = get_one(corpus, "test_type_double");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "double");
+    CHECK(parameters[0].location == "framebase+8|framebase+16");
+  }
+
+  // Unsigned
+
+  SUBCASE("one unsigned int") {
+    auto funcs = get_one(corpus, "test_type_unsigned_int");
+    REQUIRE(funcs.size() == 1);
+    auto const& parameters = funcs[0].parameters;
+    CHECK(parameters[0].type == "unsigned int");
     CHECK(parameters[0].location == "%rdi");
   }
 }
--- test/source/libs/allocation.cpp
+++ test/source/libs/allocation.cpp
@@ -1,3 +1,13 @@
 // Functions to test register allocation
 
-void f1(int x) {}
+#include <iostream>
+
+void test_type_int(int x) {}
+void test_type_unsigned_int(unsigned int x) {}
+void test_type_string(std::string x) {}
+void test_type_char(char x) {}
+void test_type_boolean(bool x) {}
+void test_type_float(float x) {}
+void test_type_double(double x) {}
+void test_type_void() {}
+void test_type_wchar_t(wchar_t x) {}
