914535d9b37a3dd69f1574ecad9d5edbc36678de
Tim Haines <thaines.astro@gmail.com>
2021-07-02 20:50:15 -0500

    Refactor getRegisterClassFromType to use Dyninst for type processing
    This also adds a first-pass at handling some cases for vectors of
    floating point types.

--- source/parser/x86_64/x86_64.cpp
+++ source/parser/x86_64/x86_64.cpp
@@ -15,6 +15,7 @@
 #include "Symtab.h"
 #include "Type.h"
 #include "allocators.hpp"
+#include "classifiers.hpp"
 #include "smeagle/parameter.h"
 #include "type_checker.hpp"
 
@@ -67,74 +68,12 @@ namespace smeagle::x86_64 {
   }
 
   // Get register class given the argument type
-  std::pair<RegisterClass, RegisterClass> getRegisterClassFromType(st::Type *paramType) {
-    // Remove top-level typedef
-    paramType = remove_typedef(paramType);
+  Class getRegisterClassFromType(st::Type *paramType) {
+    auto [base_type, ptr_cnt] = unwrap_underlying_type(paramType);
 
-    // If it's a pointer, remove it
-    if (is_indirect(paramType->getDataClass())) {
-      paramType = deref(paramType);
-      paramType = remove_typedef(paramType);
+    if (auto *t = base_type->getScalarType()) {
+      return classify(t, ptr_cnt);
     }
-
-    // Now get a string version of the type
-    std::string paramTypeString = paramType->getName();
-
-    // Signed and unsigned Bool,char,short,int,long,long long, and pointers
-    std::regex checkinteger("(int|char|short|long|pointer|bool)");
-
-    // Is it a constant?
-    std::regex checkconstant("(const)");
-
-    // float,double,_Decimal32,_Decimal64and__m64are in class SSE.
-    std::regex checksse("(double|decimal|float|Decimal|m64)");
-
-    // __float128 and__m128are split into two halves, least significant SSE,
-    // most significant in SSEUP.
-    std::regex checksseup("(m128|float128)");
-
-    // The 64-bit mantissa of arguments of type long double belongs to classX87
-    // the 16-bit exponent plus 6 bytes of padding belongs to class X87UP
-    std::regex checklongdouble("(long|double)");
-
-    // A variable of type complex long double is classified as type COMPLEX_X87
-    std::regex checkcomplexlong("(complex long double)");
-
-    // We will return a vector of classes
-    std::pair<RegisterClass, RegisterClass> regClasses;
-
-    // Does the type string match one of the types?
-    bool isinteger = (std::regex_search(paramTypeString, checkinteger));
-    bool isconst = (std::regex_search(paramTypeString, checkconstant));
-    bool issse = (std::regex_search(paramTypeString, checksse));
-    bool issseup = (std::regex_search(paramTypeString, checksseup));
-    bool islongdouble = (std::regex_search(paramTypeString, checklongdouble));
-    bool iscomplexlong = (std::regex_search(paramTypeString, checkcomplexlong));
-
-    // A parameter can have more than one class!
-    if (isconst) {
-      regClasses = {RegisterClass::MEMORY, RegisterClass::NO_CLASS};
-    }
-    if (issseup) {
-      regClasses = {RegisterClass::SSE, RegisterClass::SSEUP};
-    }
-    if (issse) {
-      regClasses = {RegisterClass::SSE, RegisterClass::NO_CLASS};
-    }
-    if (isinteger) {
-      regClasses = {RegisterClass::INTEGER, RegisterClass::NO_CLASS};
-    }
-    if (iscomplexlong) {
-      regClasses = {RegisterClass::COMPLEX_X87, RegisterClass::NO_CLASS};
-    }
-    if (islongdouble) {
-      regClasses = {RegisterClass::X87, RegisterClass::X87UP};
-    }
-
-    // The classification of aggregate (structures and arrays) and union types worksas follows:
-    // TODO need to look for struct / arrays?
-    // page 18 of abi document
-    return regClasses;
   }
 
   std::vector<parameter> parse_parameters(st::Symbol *symbol) {
